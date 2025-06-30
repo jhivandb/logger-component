8ed2d041f06d7899d767f00d114522c9a20576f4
jhivandb <jhivanb@gmail.com>
2025-06-30 16:12:26 +0530

    control chars

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ var logRows = [][]string{
 		"2025-06-24T07:08:43.327Z",
 		"ERROR",
 		"An error occurred while syncing the employee information.",
-		`{"module":"wso2/par_app.utils","error":{"causes":[],"message":"Error while executing SQL query: \n    DELETE FROM
-  REDACTED WHERE REDACTED =  ? ;\n. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table
-  'REDACTED'.","detail":{"errorCode":1142,"sqlState":"42000"},"stackTrace":[]}}`,
+		`{"module":"wso2/par_app.utils","error":{"causes":[],"message":"Error while executing SQL query: 
+    DELETE FROM REDACTED WHERE REDACTED =  ? ;
+. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.","detail":{"errorCode":1142,"sqlState":"42000"},"stackTrace":[]}}`,
 		"v1.0",
 		"3140f2f9-3ed4-4476-b1f4-c40e740682c9",
 	},
@@ -23,7 +23,9 @@ var logRows = [][]string{
 		"2025-06-24T06:53:21.577Z",
 		"ERROR",
 		"An error occurred while syncing the employee information.",
-		`{\"module\":\"wso2\/par_app.utils\",\"error\":{\"causes\":[],\"message\":\"Error while executing SQL query: \n    DELETE FROM REDACTED WHERE REDACTED =  ? ;\n. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.\",\"detail\":{\"errorCode\":1142,\"sqlState\":\"42000\"},\"stackTrace\":[]}}`,
+		`{"module":"wso2/par_app.utils","error":{"causes":[],"message":"Error while executing SQL query: 
+    DELETE FROM REDACTED WHERE REDACTED =  ? ;
+. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.","detail":{"errorCode":1142,"sqlState":"42000"},"stackTrace":[]}}`,
 		"v1.0",
 		"3140f2f9-3ed4-4476-b1f4-c40e740682c9",
 	},
@@ -31,7 +33,9 @@ var logRows = [][]string{
 		"2025-06-24T06:50:34.277Z",
 		"ERROR",
 		"An error occurred while syncing the employee information.",
-		`{\"module\":\"wso2\/par_app.utils\",\"error\":{\"causes\":[],\"message\":\"Error while executing SQL query: \n    DELETE FROM REDACTED WHERE REDACTED =  ? ;\n. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.\",\"detail\":{\"errorCode\":1142,\"sqlState\":\"42000\"},\"stackTrace\":[]}}`,
+		`{"module":"wso2/par_app.utils","error":{"causes":[],"message":"Error while executing SQL query: 
+    DELETE FROM REDACTED WHERE REDACTED =  ? ;
+. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.","detail":{"errorCode":1142,"sqlState":"42000"},"stackTrace":[]}}`,
 		"v1.0",
 		"3140f2f9-3ed4-4476-b1f4-c40e740682c9",
 	},
@@ -39,7 +43,9 @@ var logRows = [][]string{
 		"2025-06-24T05:33:16.920Z",
 		"ERROR",
 		"An error occurred while syncing the employee information.",
-		`{\"module\":\"wso2\/par_app.utils\",\"error\":{\"causes\":[],\"message\":\"Error while executing SQL query: \n    DELETE FROM REDACTED WHERE REDACTED =  ? ;\n. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.\",\"detail\":{\"errorCode\":1142,\"sqlState\":\"42000\"},\"stackTrace\":[]}}`,
+		`{"module":"wso2/par_app.utils","error":{"causes":[],"message":"Error while executing SQL query: 
+    DELETE FROM REDACTED WHERE REDACTED =  ? ;
+. DELETE command denied to user 'REDACTED'@'00.0.0.00' for table 'REDACTED'.","detail":{"errorCode":1142,"sqlState":"42000"},"stackTrace":[]}}`,
 		"v1.0",
 		"3140f2f9-3ed4-4476-b1f4-c40e740682c9",
 	},
